f51ffeb6b5eb6154f7ce424c82eb7b21c03c3b42
chrispetito <christopherpetito@gmail.com>
2019-04-22 13:25:49 -0700

    add basic component content and copy

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+function HeaderTitle() {
+  return (
+    <div>
+      <h3>Lambda School</h3>
+      <h4>@LambdaSchool</h4>
+      <h4>22 Jan</h4>
+    </div>
+  );
+}
+
+export default HeaderTitle;
