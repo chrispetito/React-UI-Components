9132b8b66b3787cdc32d79d4d0ea986cdf58cf54
chrispetito <christopherpetito@gmail.com>
2019-04-23 14:45:00 -0700

    add formatting to calculator, refactor app.js

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -3,36 +3,38 @@ import "./App.css";
 // import IndDisplayButton from "./components/ButtonComponents/action";
 import ActionButton from "./components/DisplayComponents/CalculatorDisplay";
 import NumberButton from "./components/ButtonComponents/NumberButton";
+import DisplayButton from './components/DisplayComponents/CalculatorDisplay';
 
 const App = () => {
   return (
-    <div class="container">
-      <ActionButton buttonStyle="action1" text="0" />
-      <div class="numbers-container">
+    <div className="container">
+      <div></div>
+      <DisplayButton buttonStyle="action1" text="0" />
+      <div className="numbers-container">
         <ActionButton buttonStyle="action2" text="clear" />
-        <NumberButton buttonStyle="number" text="1" />
+        <ActionButton buttonStyle="display-button" text="÷" />
       </div>
-      <div class="numbers-container">
-        <NumberButton buttonStyle="number" text="1" />
-        <NumberButton buttonStyle="number" text="2" />
-        <NumberButton buttonStyle="number" text="3" />
-        <NumberButton buttonStyle="number" text="1" />
-      </div>
-      <div class="numbers-container">
-        <NumberButton buttonStyle="number" text="4" />
-        <NumberButton buttonStyle="number" text="5" />
-        <NumberButton buttonStyle="number" text="6" />
-        <NumberButton buttonStyle="number" text="1" />
-      </div>
-      <div class="numbers-container">
+      <div className="numbers-container">
         <NumberButton buttonStyle="number" text="7" />
         <NumberButton buttonStyle="number" text="8" />
         <NumberButton buttonStyle="number" text="9" />
-        <NumberButton buttonStyle="number" text="1" />
+        <ActionButton buttonStyle="display-button" text="×" />
       </div>
-      <div class="numbers-container">
+      <div className="numbers-container">
+        <NumberButton buttonStyle="number" text="4" />
+        <NumberButton buttonStyle="number" text="5" />
+        <NumberButton buttonStyle="number" text="6" />
+        <ActionButton buttonStyle="display-button" text="−" />
+      </div>
+      <div className="numbers-container">
+        <NumberButton buttonStyle="number" text="1" />
+        <NumberButton buttonStyle="number" text="2" />
+        <NumberButton buttonStyle="number" text="3" />
+        <ActionButton buttonStyle="display-button" text="+" />
+      </div>
+      <div className="numbers-container">
         <ActionButton buttonStyle="action2" text="0" />
-        <NumberButton buttonStyle="number" text="8" />
+        <ActionButton buttonStyle="display-button" text="=" />
       </div>
     </div>
   );
