2146daaa472137226f97c483e9cb015096b87c56
chrispetito <christopherpetito@gmail.com>
2019-04-23 13:59:27 -0700

    complete basic layout of calculator

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,21 +1,39 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+// import IndDisplayButton from "./components/ButtonComponents/action";
+import ActionButton from "./components/DisplayComponents/CalculatorDisplay";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div class="container">
+      <ActionButton buttonStyle="action1" text="0" />
+      <div class="numbers-container">
+        <ActionButton buttonStyle="action2" text="clear" />
+        <NumberButton buttonStyle="number" text="1" />
+      </div>
+      <div class="numbers-container">
+        <NumberButton buttonStyle="number" text="1" />
+        <NumberButton buttonStyle="number" text="2" />
+        <NumberButton buttonStyle="number" text="3" />
+        <NumberButton buttonStyle="number" text="1" />
+      </div>
+      <div class="numbers-container">
+        <NumberButton buttonStyle="number" text="4" />
+        <NumberButton buttonStyle="number" text="5" />
+        <NumberButton buttonStyle="number" text="6" />
+        <NumberButton buttonStyle="number" text="1" />
+      </div>
+      <div class="numbers-container">
+        <NumberButton buttonStyle="number" text="7" />
+        <NumberButton buttonStyle="number" text="8" />
+        <NumberButton buttonStyle="number" text="9" />
+        <NumberButton buttonStyle="number" text="1" />
+      </div>
+      <div class="numbers-container">
+        <ActionButton buttonStyle="action2" text="0" />
+        <NumberButton buttonStyle="number" text="8" />
+      </div>
     </div>
   );
 };
